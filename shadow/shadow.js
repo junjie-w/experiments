3b21a11aabefe7e031e0e506ad944b15283d1b17
Junjie Wu <>
2021-05-16 17:40:56 +0200

    deleted comments

--- shadow/shadow.js
+++ shadow/shadow.js
@@ -1,18 +1,3 @@
-// let bg = document.getElementById("bg");
-// let moon = document.getElementById("moon");
-// let mountain = document.getElementById("mountain");
-// let road = document.getElementById("road");
-// let text = document.getElementById("text");
-
-// window.addEventListener("scroll", () => {
-//     let value = window.scrollY;
-//     bg.style.top = value * 0.5 + 'px';
-//     moon.style.left = -value * 0.5 + 'px';
-//     mountain.style.top = -value * 0.15 + 'px';
-//     road.style.top = value * 0.15 + 'px';
-//     text.style.top = value * 1 + 'px';
-// })
-
 let snowman = document.querySelector("#snowmanAnimation");
 let yellowLight = document.querySelector("#yellow-light");
 let redLight = document.querySelector("#red-light");
